2a015ba218d21aea13992882d21711916fa50f62
sofiaaperezz <spv34@alu.ua.es>
2025-02-21 10:28:05 +0100

    Update profesor.py

--- profesor.py
+++ profesor.py
@@ -6,3 +6,25 @@ class Profesor:
         self.especialidad = especialidad
         self.cursos_asignados = cursos_asignados
         Profesor.total_profesores += 1  # Aumenta el contador cada vez que se crea un profesor
+
+    def asignar_curso(self, curso: str):
+        self.cursos_asignados.append(curso)
+
+    def mostrar_informacion(self):
+        print(f"Nombre: {self.nombre}")
+        print (f"Especialidad: {self.especialidad}")
+        print(f"Cursos asignados: {self.cursos_asignados}")
+
+    def __str__(self):
+        return f"Profesor {self.nombre}, Especialidad: {self.especialidad}, Cursos asignados: {self.cursos_asignados}"
+
+    def __repr__(self):
+        return f"Profesor({self.nombre}, {self.especialidad}, {self.cursos_asignados})"
+
+    @classmethod
+    def desde_tupla(cls, tupla):
+        return cls(tupla)
+
+    @staticmethod
+    def esta_disponible_para_nuevo_curso():
+        return True
