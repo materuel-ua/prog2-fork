6dec4d52d2eae91b1af56ebaa9f5ecbf71efb400
Carlos Salas Alarcon <csa94@alu.ua.es>
2025-02-21 09:52:16 +0100

    Update aula.py

--- aula.py
+++ aula.py
@@ -5,4 +5,24 @@ class Aula:
         self.numero = numero
         self.capacidad = capacidad
         self.recursos = recursos
+        self.reservada= False
         Aula.total_aulas += 1  # Aumenta el contador cada vez que se crea un aula
+
+    def mostrar_detalles(self) -> str:
+        return (f'Este colegio tiene {type(self).total_aulas} clases. Esta clase en concreto tiene {self.capacidad} estudiantes \
+                su numero es {self.numero} y tiene estos recursos: {self.recursos}')
+    def reservar_aula(self,):
+        self.reservada= True
+        print('La clase esta reservada.')
+
+    @ classmethod
+    def desde_tupla(cls, tupla) -> object:
+        return cls(tupla[0],tupla[1],tupla[2])
+    def es_adecuada_para_clase(nombre):
+        print(f'{nombre} sera una gran adicion a la clase.')
+
+    def __str__(self):
+        return f'{type(self).__name__}, {type(self).total_aulas}, {self.numero}, {self.capacidad}, {self.recursos}'
+    def __repr__(self):
+        return f'{type(self).__name__}, (total_aulas = {type(self).total_aulas}, numero= {self.numero}\
+    capacidad = {self.capacidad}, recursos = {self.recursos}'
