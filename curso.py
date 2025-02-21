12532c285365353688b23cf649d088cc697174fc
MohammedAli-Biar <maab16@alu.ua.es>
2025-02-21 09:49:18 +0100

    Update curso.py

--- curso.py
+++ curso.py
@@ -14,18 +14,18 @@ class Curso:
     def __repr__(self) -> str:
         return f'{type(self)}(nombre = "{self.nombre}", codigo = "{self.codigo}", descripcion = "{self.descripcion}")'
     
-    def mostrar_detalles(self):
+    def mostrar_detalles(self) -> str:
 
         return self.descripcion
     
     @classmethod
 
-    def desde_tupla(cls,datos):
+    def desde_tupla(cls,datos:tuple) -> object:
 
         return cls(datos[0],datos[1],datos[2])
     
     @staticmethod
 
-    def es_curso_abierto():
+    def es_curso_abierto() -> str:
 
         return 'El curso esta abierto para el publico.'
