eaf66b5732653edf6c14947da8569e662a8db976
vicenteprzz <vicentepourtau@gmail.com>
2025-02-21 10:23:59 +0100

    horario
    fff

--- horario.py
+++ horario.py
@@ -7,3 +7,10 @@ class Horario:
         self.hora_inicio = hora_inicio
         self.hora_fin = hora_fin
         Horario.total_horarios += 1  # Aumenta el contador cada vez que se crea un horario
+    def mostrar_horario(self):
+        return f'{self.curso} - {self.dia}: {self.hora_inicio} - {self.hora_fin}'
+    def modificar_horario(self,nuevo_horario: dict):
+        self.hora_inicio = nuevo_horario.get("horario_inicio",self.hora_inicio)
+        self.hora_fin = nuevo_horario.get("horario_fin",self.hora_fin)
+    def __str__(self):
+        return self.mostrar_horario()
