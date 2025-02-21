dff7248e9a07e16acc0e550330b0ad4f53215817
dariomarimonn <dariomarimon04@gmail.com>
2025-02-21 10:00:35 +0100

    Update horario.py
    actualizado

--- horario.py
+++ horario.py
@@ -7,3 +7,5 @@ class Horario:
         self.hora_inicio = hora_inicio
         self.hora_fin = hora_fin
         Horario.total_horarios += 1  # Aumenta el contador cada vez que se crea un horario
+
+        #cauyvwqiuodgvbaedfyiwegfouwqvbefuofhiberofboiherfbihyobwepobweripofbiherfbhiure
