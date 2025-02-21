8e4d3d298640f390c31b7360d03937a690186f4a
Marcos Hernández Juárez <mhj7@alu.ua.es>
2025-02-21 10:00:21 +0100

    clase actualizada
    jdaghdjg

--- curso.py
+++ curso.py
@@ -6,3 +6,13 @@ class Curso:
         self.codigo = codigo
         self.descripcion = descripcion
         Curso.total_cursos += 1  # Aumenta el contador cada vez que se crea un curso
+
+    def mostrar_detalles(self,nombre,codigo,descripcion):
+        print(self.nombre)
+        print(self.codigo)
+        print(self.descripcion)
+
+    def actualizar_descripción(self,nueva_descripcion):
+        nueva_descripcion=self.descripcion
+
+    def __str__(self):
